15d9a9f60fe38784668cbed7b959faf330d510e5
liulu02_dxm <liulu02@duxiaoman.com>
2022-10-23 00:31:05 +0800

    fix

--- process/create_process.c
+++ process/create_process.c
@@ -1,5 +1,15 @@
 //
 // Created by Apple on 2022/10/23.
 //
-
+#include <unistd.h>
+#include <stdio.h>
 #include "create_process.h"
+int main() {
+     pid_t p1 = fork();
+     if (p1 == 0) {
+         printf("this is father process!\n");
+     } else {
+         printf("this is child process!\n");
+     }
+     return 0;
+}
